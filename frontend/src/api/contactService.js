294b5a891ceced3fa1975be01897d5d2fe64acd6
Neisseiria <oceane.marqu@gmail.com>
2025-09-11 22:59:56 +0200

    chgmt form

--- frontend/src/api/contactService.js
+++ frontend/src/api/contactService.js
@@ -1,6 +1,6 @@
 // src/api/contactService.js
 import axios from 'axios';
-const API_URL = `${import.meta.env.VITE_API_URL}/api/contact/`;
+const API_URL = `${import.meta.env.VITE_API_URL}/contact/`;
 
 const sendMessage = (contactData) => {
   return axios.post(API_URL, contactData);
